e39eb5015b9af766803f978bd9cf8105bd4bca51
ThanhLuan150 <116501650+ThanhLuan150@users.noreply.github.com>
2023-09-29 19:38:46 +0700

    Update

--- Screens/Play.jsx
+++ Screens/Play.jsx
@@ -2,65 +2,84 @@ import { View, Text, StyleSheet, TouchableOpacity, TouchableWithoutFeedback ,Dim
 import { useState } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { useNavigation } from '@react-navigation/native';
-const Play = () => {
-     const [Language, setLanguage] = useState('en');
-     const [Index, setIndex] = useState(0);
-     const navigation = useNavigation()
-     const handleSetting = () =>{
-        navigation.navigate('Setting');
-        }
-    const Data = [ 
-        { en: "Hello", vi: "xin chào" },
-        { en: "Apple", vi: "Táo" },
-        { en: "Table", vi: "Bàn" }
-    ]
-                
-        const [languageDate, setLanguageDate] = useState(Data);
-        const [currentData, setCurrentData] = useState(languageDate[0]);
+    const Play = () => {
+    // Khai báo và khởi tạo trạng thái Language và Index sử dụng useState.
+    // Language lưu trữ ngôn ngữ hiện tại, mặc định là 'en' (tiếng Anh).
+    // Index lưu trữ chỉ mục của từ vựng hiện tại trong danh sách, mặc định là 0.
+    const [Language, setLanguage] = useState('en');
+    const [Index, setIndex] = useState(0);
 
-        const toggleLanguage = () => {
-            if (Language === 'en') {
-                setLanguage('vi');
-            } else {
-               setLanguage('en');
-         }
-            };
-            
-            
-        const nextItem = () => {
-            setIndex((Index) => (Index + 1) % languageDate.length);
-            setCurrentData(languageDate[Index]);
-            };
-            
-            
-        const previousItem = () => {
-            setIndex((Index)=> (Index -1 + languageDate.length) % languageDate.length);
-            setCurrentData(languageDate[Index]);
-        };
-            
-        const handleRemove = () => {
-        const newData = [...languageDate];
-            if (newData.length > 0) {
-                newData.splice(Index, 1);
-                setLanguageDate(newData);
-            
-            if (newData.length === 0) {
-                setCurrentData({});
-                setIndex(0);
-            } else if (Index >= newData.length) {
-                setIndex(newData.length - 1);
-                setCurrentData(newData[newData.length - 1]);
-            } else {
-                setCurrentData(newData[Index]);
-                }
-            }
-                };
-                
-            const handReset = () => {
-            setLanguageDate(Data);
-            setIndex(0); // Đặt lại chỉ mục về 0
-            setCurrentData(Data[0]); // Đặt lại currentData thành phần đầu tiên của Data
-            };
+    // Sử dụng hook useNavigation để có quyền truy cập đối tượng điều hướng.
+    const navigation = useNavigation();
+
+    // Hàm handleSetting được gọi khi người dùng chạm vào nút "Setting" để điều hướng đến màn hình "Setting".
+    const handleSetting = () => {
+    navigation.navigate('Setting');
+    }
+
+    // Mảng Data chứa các từ vựng bilingue (tiếng Anh và tiếng Việt).
+    const Data = [ 
+    { en: "Hello", vi: "xin chào" },
+    { en: "Apple", vi: "Táo" },
+    { en: "Table", vi: "Bàn" }
+    ]
+
+    // Khai báo và khởi tạo các trạng thái khác.
+    // languageDate lưu trữ danh sách từ vựng, ban đầu được khởi tạo từ mảng Data.
+    const [languageDate, setLanguageDate] = useState(Data);
+
+    // currentData lưu trữ từ vựng hiện tại được hiển thị trên giao diện, ban đầu là từ vựng đầu tiên trong danh sách.
+    const [currentData, setCurrentData] = useState(languageDate[0]);
+
+    // Hàm toggleLanguage chuyển đổi giữa ngôn ngữ hiện tại (tiếng Anh và tiếng Việt).
+    const toggleLanguage = () => {
+    if (Language === 'en') {
+        setLanguage('vi'); // Nếu đang là tiếng Anh, chuyển sang tiếng Việt.
+    } else {
+        setLanguage('en'); // Ngược lại, chuyển sang tiếng Anh.
+    }
+    };
+
+    // Hàm nextItem được gọi khi người dùng chạm vào nút "Next" để hiển thị từ vựng tiếp theo trong danh sách.
+    const nextItem = () => {
+    // Tăng chỉ mục Index và chuyển sang từ vựng tiếp theo trong danh sách (lặp lại khi đạt cuối danh sách).
+    setIndex((Index) => (Index + 1) % languageDate.length);
+    setCurrentData(languageDate[Index]);
+    };
+
+    // Hàm previousItem được gọi khi người dùng chạm vào nút "Previous" để hiển thị từ vựng trước đó trong danh sách.
+    const previousItem = () => {
+    // Giảm chỉ mục Index và chuyển sang từ vựng trước đó trong danh sách (lặp lại khi đạt đầu danh sách).
+    setIndex((Index) => (Index - 1 + languageDate.length) % languageDate.length);
+    setCurrentData(languageDate[Index]);
+    };
+
+    // Hàm handleRemove được gọi khi người dùng chạm vào nút "Remove From Deck" để xóa từ vựng hiện tại khỏi danh sách.
+    const handleRemove = () => {
+    const newData = [...languageDate];
+    if (newData.length > 0) {
+        newData.splice(Index, 1); // Xóa từ vựng tại chỉ mục Index khỏi newData.
+        setLanguageDate(newData); // Cập nhật danh sách từ vựng.
+
+        if (newData.length === 0) {
+        setCurrentData({}); // Nếu danh sách rỗng, đặt lại currentData thành trống.
+        setIndex(0); // Đặt lại chỉ mục Index về 0.
+        } else if (Index >= newData.length) {
+        setIndex(newData.length - 1); // Đặt lại Index nếu nó lớn hơn hoặc bằng độ dài mới của danh sách.
+        setCurrentData(newData[newData.length - 1]); // Đặt lại currentData thành phần cuối cùng của danh sách mới.
+        } else {
+        setCurrentData(newData[Index]); // Đặt lại currentData thành phần hiện tại.
+        }
+    }
+    };
+
+    // Hàm handReset được gọi khi người dùng chạm vào nút "Reset Deck" để đặt lại danh sách từ vựng ban đầu.
+    const handReset = () => {
+    setLanguageDate(Data); // Đặt lại danh sách từ vựng thành Data ban đầu.
+    setIndex(0); // Đặt lại chỉ mục Index về 0.
+    setCurrentData(Data[0]); // Đặt lại currentData thành phần đầu tiên của Data.
+    };
+
                 
     return(
         <View style={{ paddingBottom:20 }}>
